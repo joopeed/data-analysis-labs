3359f034731d6442150b54c19328cd232d6dbb60
João Pedro Leôncio <joopeeds@gmail.com>
2014-05-26 20:57:01 -0300

    Including first summarizations

---
+++ lab2/lab2.R
@@ -0,0 +1,8 @@
+dados <- read.table("C:\\atividade-maquinas-dsc.txt", header=TRUE)
+dados$laboratorio <- gsub("lccpeer@xmpp.ourgrid.org", "LCC1", dados$laboratorio)
+dados$laboratorio <- gsub("peer-lcc2@xmpp.ourgrid.org", "LCC2", dados$laboratorio)
+dados$laboratorio <- gsub("gaita@gaita.gmf.ufcg.edu.br", "SPLab", dados$laboratorio)
+dados$laboratorio <- gsub("peer-lsd@xmpp.ourgrid.org", "LSD", dados$laboratorio)
+table(dados$laboratorio)
+
+
