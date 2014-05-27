2df95738b21de730c1cb8bd3da82526dfe142622
João Pedro Leôncio <joopeeds@gmail.com>
2014-05-26 21:00:51 -0300

    Changing working directory

--- lab2/lab2.R
+++ lab2/lab2.R
@@ -1,8 +1,9 @@
-dados <- read.table("C:\\atividade-maquinas-dsc.txt", header=TRUE)
+dados <- read.table("atividade-maquinas-dsc.txt", header=TRUE)
 dados$laboratorio <- gsub("lccpeer@xmpp.ourgrid.org", "LCC1", dados$laboratorio)
 dados$laboratorio <- gsub("peer-lcc2@xmpp.ourgrid.org", "LCC2", dados$laboratorio)
 dados$laboratorio <- gsub("gaita@gaita.gmf.ufcg.edu.br", "SPLab", dados$laboratorio)
 dados$laboratorio <- gsub("peer-lsd@xmpp.ourgrid.org", "LSD", dados$laboratorio)
 table(dados$laboratorio)
+
 
 
